37eb9e1efe352e9531585e833136c3fcd2af8a37
rnaka <raena6@hawaii.edu>
2017-03-02 12:02:15 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Rae on 3/2/17.
+ */
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient){
+  Template.tasks.helpers({
+    tasks: function(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  })
+
+  Template.tasks.events({
+    "submit .add-task": function(event){
+      var name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createdAt: new Date()
+      });
+
+      event.target.name.value = '';
+
+      return false;
+    }
+  })
+}
